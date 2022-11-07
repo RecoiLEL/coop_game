3870d466b8cfae4e6d15ebbb267f9a938ce0f3a7
KWON DOHEON <qofnla2384@gmail.com>
2022-11-07 14:57:17 +0900

    server keep going

--- net.c
+++ net.c
@@ -4,5 +4,8 @@
 struct sockaddr_in server_sock_addr(char *ip){
     struct sockaddr_in server_addr;
     memset( &server_addr, 0, sizeof(server_addr) );
-    
+    server_addr.sin_family = AF_INET;
+    if(ip==NULL){
+        
+    }
 }
