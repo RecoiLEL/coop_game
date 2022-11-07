96dac77828a7c74445af76fac20feb9060093cea
KkK-lol <c612101406@tokushima-u.ac.jp>
2022-11-07 15:23:12 +0900

    関数名追加

--- client_main.c
+++ client_main.c
@@ -2,5 +2,12 @@
 
 int main(int argc,char *argv[])
 {
+    int		num;
+    char	name[MAX_CLIENTS][MAX_NAME_SIZE];
+    int		endFlag=1;
+    char	localHostName[]="localhost";
+    char	*serverName;
+    int		clientID;
+    
     return 0;
 }
