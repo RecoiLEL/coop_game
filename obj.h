a67ddd07d4f7193e6c37bd129a1477653535cf10
KWON DOHEON <qofnla2384@gmail.com>
2022-11-14 14:22:23 +0900

    add map data

--- obj.h
+++ obj.h
@@ -6,10 +6,24 @@
 struct Player {
     SDL_Rect position;
     SDL_Texture *texture;
+    int map_x;
+    int map_y;
+    int offset_x;
+    int offset_y;
+    int velocity_x;
+    int velocity_y;
+    int pixel_x;
+    int pixel_y;
     int left, right, jump;
     int can_jump;
     int left_button, right_button, jump_button;
     int dead, clear;
 };
+
+struct Map {
+    SDL_Rect MapRect;
+    SDL_Texture *Mtexture;
+    int MAPCHIP;
+}
 
 #endif
