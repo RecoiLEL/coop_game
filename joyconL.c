f64f1d18e318c3361ab7fb7660c2493a09058f82
tk283 <tktsubasa@jcom.zaq.ne.jp>
2022-11-14 14:53:44 +0900

    joycon

--- joyconL.c
+++ joyconL.c
@@ -4,16 +4,18 @@ joyconlib_t jc;
 joycon_open(&jc, JOYCON_L);   //Joy-Conのオープン
 
 joycon_get_state(&jc);  //Joy-Conの操作
+if(jc.button.btn.X){
+  // Xが押されているとき
+}
+if(jc.button.btn.B){
+  // Xが押されているとき
+}
+if(jc.button.btn.Y){
+  // Xが押されているとき
+}
 
 joycon_close(&jc);  //Joy-Conのクローズ
 
 joycon_elapsed et;
 joycon_get_button_elapsed(&jc, &et);
 
-joycon_get_state(&jc);
-if(jc.button.btn.X){
-  // Xが押されているとき
-}
-if(jc.stick.y < 0.0){
-  // スティックが下に…
-}
