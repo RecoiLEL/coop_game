b34b5bf1c9427f2ef9fa691106086f55caa2d9d9
KWON DOHEON <qofnla2384@gmail.com>
2022-11-14 14:10:50 +0900

    add key state

--- constant.h
+++ constant.h
@@ -2,14 +2,17 @@
 #define CONSTANS_H
 
 #include "stdbool.h"
-#include "joyconlib.h"
+#include <joyconlib.h>
 
 #define MAX_PLAYER 2
 #define HEAD_JUMP 20
 #define JUMP 15
 #define GRAV 9.8
-#define RIGHT
-#define LEFT
-#define UP
+#define R_RIGHT_BUTTON
+#define R_LEFT_BUTTON
+#define R_JUMP_BUTTON
+#define L_RIGHT_BUTTON
+#define L_LEFT_BUTTON
+#define L_JUMP_BUTTON
 
 #endif
