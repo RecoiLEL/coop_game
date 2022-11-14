68a5ee805cfbad02cd91dc6bf6615c177a363781
KkK-lol <c612101406@tokushima-u.ac.jp>
2022-11-14 14:24:18 +0900

    タイマー関数部分実装

--- client_main.c
+++ client_main.c
@@ -37,8 +37,27 @@ int main(int argc,char *argv[])
     /* ウインドウの初期化 */
 	if(InitWindows(clientID,num,name)==-1){
 		fprintf(stderr,"setup failed : InitWindows\n");
-		return -1;
+    goto DESTROYALL;
 	}
+
+  /* スレッドセーフな整数変数を扱う 
+     * スレッドの生存確認(0以下でスレッドを終了させる)と
+     * 簡易フレームカウンタとして利用 */
+    SDL_atomic_t atm;
+    SDL_AtomicSet(&atm, 1);
+    /* スレッド */
+    SDL_Thread* thread = SDL_CreateThread(InputEvent, "InputEvent", &atm);
+    if (thread == NULL) {
+        PrintError(SDL_GetError());
+        goto DESTROYALL;
+    }
+    SDL_DetachThread(thread);
+    /* タイマー */
+    SDL_TimerID timer = SDL_AddTimer(100, AniTimer, &atm);
+    if (timer == 0) {
+        PrintError(SDL_GetError());
+        goto RELEASETHREAD;
+    }
 
     /* メインイベントループ */
     while(endFlag){
@@ -46,9 +65,26 @@ int main(int argc,char *argv[])
 		endFlag = SendRecvManager();
     };
 
-    /* 終了処理 */
+  /* 終了処理 */
+  SDL_RemoveTimer(timer);
+RELEASETHREAD:
+    SDL_AtomicSet(&atm, -10);
+DESTROYALL:
 	DestroyWindow();
 	CloseSoc();
 
     return 0;
+}
+
+/* エラーメッセージ表示
+ *
+ * 引数
+ *   str: エラーメッセージ
+ *
+ * 返値: -1
+ */
+int PrintError(const char* str)
+{
+    fprintf(stderr, "%s\n", str);
+    return -1;
 }
